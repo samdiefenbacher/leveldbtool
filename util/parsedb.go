db1cc2afcc32fc09578411cf687c3b3e7a8a24d9
danhale-git <danhale87@gmail.com>
2021-08-08 20:47:09 +0100

    Call state palette to get block data.

--- util/parsedb.go
+++ util/parsedb.go
@@ -3,10 +3,13 @@ package util
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"math"
+
+	"github.com/midnightfreddie/nbt2json"
 )
 
 func read(r io.Reader, data interface{}) error {
@@ -14,12 +17,31 @@ func read(r io.Reader, data interface{}) error {
 }
 
 type NBTTag struct {
-	Type  byte
-	Name  string
-	Value interface{}
+	Type  byte        `json:"tagType"`
+	Name  string      `json:"name"`
+	Value interface{} `json:"value"`
+}
+
+func (n *NBTTag) BlockID() string {
+	//	fmt.Printf("%+v\n", n)
+	if vs, ok := n.Value.([]interface{}); ok {
+		for _, t := range vs {
+			if tMap, ok := t.(map[string]interface{}); ok {
+				if tMap["name"] == "name" {
+					return tMap["value"].(string)
+				}
+			}
+		}
+	} else {
+		fmt.Println("failed to convert to NBTTag")
+	}
+
+	return ""
 }
 
 func ParseSubChunk(data []byte) {
+	//printNBTJSON(data)
+
 	r := bytes.NewReader(data)
 	fmt.Println("reader length:", r.Len())
 
@@ -42,29 +64,85 @@ func ParseSubChunk(data []byte) {
 	}
 	fmt.Println("storageCount:", storageCount) //DEBUG
 
-	var bitsPerBlock, storageVersion, blocksPerWord int
-
-	var i int8
-	for i = 0; i < storageCount; i++ {
+	var s int8
+	for s = 0; s < storageCount; s++ {
 		var bitsPerBlockAndVersion byte
 		if err := read(r, &bitsPerBlockAndVersion); err != nil {
 			log.Fatal(err)
 		}
 
-		bitsPerBlock = int(bitsPerBlockAndVersion >> 1)
-		storageVersion = int(bitsPerBlockAndVersion & 1)
+		bitsPerBlock := int(bitsPerBlockAndVersion >> 1)
+		storageVersion := int(bitsPerBlockAndVersion & 1)
 		fmt.Println("bitsPerBlock:", float64(bitsPerBlock)) //DEBUG
 		fmt.Println("storageVersion:", storageVersion)      //DEBUG
 
-		blocksPerWord = int(math.Floor(32.0 / float64(bitsPerBlock)))
+		//indices := make([]int, 0)
 
-		fmt.Println("blocksPerWord:", blocksPerWord) //DEBUG
+		indices, x, y, z := getBlockDataIndices(r, bitsPerBlock)
+
+		/*testMap := make(map[int]bool)
+		for _, i := range indices {
+			testMap[i] = true
+		}
+		for k := range testMap {
+			fmt.Println(k)
+		}*/
+
+		fmt.Println("index count:", len(indices))
+
+		var paletteSize int32
+		if err := read(r, &paletteSize); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("paletteSize:", paletteSize) //DEBUG
+
+		fmt.Println("reader length:", r.Len())
+		fmt.Println("data length:", len(data))
+
+		remainingBytes, err := io.ReadAll(r)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		j, err := nbt2json.Nbt2Json(remainingBytes, "")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		nbt := struct {
+			Nbt []NBTTag
+		}{}
+		//var nbtTags []NBTTag
+		if err := json.Unmarshal(j, &nbt); err != nil {
+			log.Fatal(err)
+		}
+
+		for i := range indices {
+			if i > 5 {
+				break
+			}
+			index := indices[i]
+			x, y, z := x[i], y[i], z[i]
+
+			fmt.Printf("(%d, %d, %d)", x, y, z)
+			fmt.Printf(" - %s\n", nbt.Nbt[index].BlockID())
+		}
 	}
+}
+
+func getBlockDataIndices(r io.Reader, bitsPerBlock int) (indices, x, y, z []int) {
+	blocksPerWord := int(math.Floor(32.0 / float64(bitsPerBlock)))
+
+	fmt.Println("blocksPerWord:", blocksPerWord) //DEBUG
 
 	wordCount := int(math.Ceil(4096 / float64(blocksPerWord)))
 	fmt.Println("wordCount:", wordCount) //DEBUG
 
-	indices := make([]int, 0)
+	indices = make([]int, 4096)
+	x = make([]int, 4096)
+	y = make([]int, 4096)
+	z = make([]int, 4096)
+	idx := 0
 
 	for w := 0; w < wordCount; w++ {
 		word := make([]byte, 4)
@@ -72,57 +150,43 @@ func ParseSubChunk(data []byte) {
 			log.Fatal(err)
 		}
 
-		indices = append(indices, getBlockDataIndices(word, bitsPerBlock)...)
-	}
+		// TODO: Also get the block position
 
-	/*testMap := make(map[int]bool)
-	for _, i := range indices {
-		testMap[i] = true
-	}
-	for k := range testMap {
-		fmt.Println(k)
-	}*/
+		// Might need to use a bit reader here if numbers other than 4 or 8 come up
+		switch bitsPerBlock {
+		case 4:
+			for _, b := range word {
+				first := b >> 4
+				x[idx], y[idx], z[idx] = blockPosition(idx)
+				indices[idx] = int(first)
+				idx++
 
-	fmt.Println("index count:", len(indices))
-
-	var paletteSize int32
-	if err := read(r, &paletteSize); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("paletteSize:", paletteSize) //DEBUG
-
-	fmt.Println("reader length:", r.Len())
-	fmt.Println("data length:", len(data))
-}
-
-func getBlockDataIndices(word []byte, bitsPerBlock int) []int {
-	indices := make([]int, 0)
-
-	// Might need to use a bit reader here if numbers other than 4 or 8 come up
-	switch bitsPerBlock {
-	case 4:
-		for _, b := range word {
-			first := b >> 4
-			second := (b << 4) >> 4
-			indices = append(indices, int(first), int(second))
+				second := (b << 4) >> 4
+				x[idx], y[idx], z[idx] = blockPosition(idx)
+				indices[idx] = int(second)
+				idx++
+			}
+		default:
+			log.Panicf("unhandled bits per block '%d'", bitsPerBlock)
 		}
-	default:
-		log.Panicf("unhandled bits per block '%d'", bitsPerBlock)
 	}
 
-	return indices
+	return indices, x, y, z
 }
 
-func printNBTJSON() {
-	/*remainingBytes, err := io.ReadAll(r)
+func blockPosition(increment int) (x, y, z int) {
+	x = (increment >> 8) & 0xF
+	y = increment & 0xF
+	z = (increment >> 4) & 0xF
+
+	return
+}
+
+func printNBTJSON(b []byte) {
+	j, err := nbt2json.Nbt2Json(b, "")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	j, err := nbt2json.Nbt2Json(remainingBytes, "")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(string(j))*/
+	fmt.Println(string(j))
 }
